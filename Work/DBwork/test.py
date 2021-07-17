50a9b8bfbfc18b163b209fc94d30d761f6129121
chenshi <305675408@qq.com>
2021-07-17 22:13:07 +0800

    test

--- Work/DBwork/test.py
+++ Work/DBwork/test.py
@@ -1,5 +1,5 @@
 import json
-data={"name":"aaa","age":17,"ulist": [{"人数":12,"user":"asasj"}]}
+data = {"name": "aaa", "age": 17, "ulist": [{"人数": 12, "user": "asasj"}]}
 print(type(data))
 for key in data:
     print(data[key])
